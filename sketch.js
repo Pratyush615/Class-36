d139e498afaaaf0917529a4f9b690291c25a9c5a
Pratyush615 <mathurpm@gmail.com>
2021-02-17 16:41:41 -0500

    Made a real time database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
 var ball;
-
+var ballPosition,database;
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    ballPosition = database.ref('ball/position');
+    ballPosition.on("value", readPosition, showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
+//getting the values from the program/browser and updating it to the database
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x': position.x + x,
+        'y' : position.y + y
+    })
+}
+//reading the values from database and assigning it to the program
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;    
+}
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function showError(){
+    console.log("An error occured in reading the database!")
 }
