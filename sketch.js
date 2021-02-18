3853f8278686e8a707f54cb69d0b51b12a531715
Pratyush615 <mathurpm@gmail.com>
2021-02-18 16:58:07 -0500

    Added multiplayer features

--- sketch.js
+++ sketch.js
@@ -1,45 +1,15 @@
-var ball;
-var ballPosition,database;
+var gameState = 0;
+var playerCount;
+var database;
+var form, player, game;
 function setup(){
     database = firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-
-    ballPosition = database.ref('ball/position');
-    ballPosition.on("value", readPosition, showError);
+   game = new Game();
+   game.getState();
+   game.start();
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
-}
-//getting the values from the program/browser and updating it to the database
-function writePosition(x,y){
-    database.ref('ball/position').set({
-        'x': position.x + x,
-        'y' : position.y + y
-    })
-}
-//reading the values from database and assigning it to the program
-function readPosition(data){
-    position = data.val();
-    ball.x = position.x;
-    ball.y = position.y;    
-}
-
-function showError(){
-    console.log("An error occured in reading the database!")
+    
 }
